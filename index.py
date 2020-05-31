92f6e6bb046f416985c1ffece22d661e92b3e1f5
dimon4egkl <50290945+dimon4egkl@users.noreply.github.com>
2020-05-31 16:37:30 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
-#!/usr/bin/python3.7
-from flask import Flask
+#!/usr/bin python3.7
+import cglib
+cglib.enable()
+print "Content-type: text/html" 
+print ()
+print "<html><body>Test Page</body></html>"
+#from flask import Flask
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>TEST Flask app</h1'
+#@app.route('/')
+#def index():
+ #   return '<h1>TEST Flask app</h1'
 
-if __name__=='__main__':
-    app.run()
+#if __name__=='__main__':
+    #app.run()
