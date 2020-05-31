8158aeea3303b3cd6f0d94c05a155d628cea0164
dimon4egkl <50290945+dimon4egkl@users.noreply.github.com>
2020-05-31 18:49:07 +0300

    Update index.py

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ import telebot
 API_TOKEN = config.TOKEN
 
 WEBHOOK_HOST = '185.253.218.184'
-WEBHOOK_PORT = 80  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_PORT = 88  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '185.253.218.184'  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
