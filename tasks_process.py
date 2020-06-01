05f60600823dcf288546031af6a3d2f1681d702c
dimon4egkl <50290945+dimon4egkl@users.noreply.github.com>
2020-06-01 07:06:47 +0300

    Add files via upload

---
+++ tasks_process.py
@@ -0,0 +1,24 @@
+import sqlite3
+import datetime
+
+db = sqlite3.connect("server.sqlite3")
+sql = db.cursor()
+
+# sql.execute("SELECT id, present_day_task_completeness, wakeup_completeness FROM users")
+# #
+# # tasks = sql.fetchall()
+# # for task in tasks:
+# #     print(task)
+# #     sql.execute("UPDATE users SET prev_day_task_completeness =?, present_day_task_completeness=?  WHERE id =? ",(task[1],0,task[0],))
+# #     db.commit()
+# # print("done")
+
+sql.execute("SELECT id, present_day_task_completeness, wakeup_completeness FROM users")
+
+tasks = sql.fetchall()
+for task in tasks:
+    print(task)
+    sql.execute("UPDATE users SET wakeup_completeness =?  WHERE id =? ",(1,task[0],))
+    print(task)
+    db.commit()
+print("done")
