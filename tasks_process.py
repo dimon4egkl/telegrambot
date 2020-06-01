646e82f1f7c59615f4923098515d5fef4c86ce9b
dimon4egkl <50290945+dimon4egkl@users.noreply.github.com>
2020-06-01 07:21:43 +0300

    Update tasks_process.py

--- tasks_process.py
+++ tasks_process.py
@@ -18,6 +18,9 @@ sql.execute("SELECT * FROM users")
 tasks = sql.fetchall()
 for task in tasks:
     print(task)
-    sql.execute("UPDATE users SET prev_day_task_completeness =?  WHERE id =? ",(1,task[0],))
+     if task[1] == 'Vitalii' or task[9] =="Діма" or task[9] =="Dasha" or task[9]=="Volodymyr":
+         sql.execute("UPDATE users SET prev_day_task_completeness =?  WHERE id =? ",(1,task[0],))
+     else:
+         sql.execute("UPDATE users SET prev_day_task_completeness =?,wakeup_completeness =?  WHERE id =? ",(1,0,task[0],))
     db.commit()
 print("done")
