72b4da0e1837fe225fcc54628df84e7736fd0c50
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-01 21:30:09 +0300

    add task reminder

---
+++ reminder_task.py
@@ -0,0 +1,23 @@
+import time
+import sqlite3
+import telebot
+import config
+import datetime
+
+bot = telebot.TeleBot(config.TOKEN)
+db = sqlite3.connect("server.sqlite3")
+sql = db.cursor()
+# while True:
+date = datetime.datetime.now().time()
+hour = date.hour #+ 7
+    # додати 7 годин коли закідати в git
+if hour == 18 or hour ==21:
+    sql.execute("SELECT id, present_day_task_completeness FROM users")
+    users = sql.fetchall()
+    for user in users:
+        #if user[0] == 404063513 :
+        if user[1] == 0 :
+            bot.send_message(user[0], "Пссс Пссс. Тут тебе цей... завдання чекає. Рекомендую приділити пару хвилин, щоб його виконати. А тоді повертайся до своїх корисних і приємних справ")
+   # time.sleep()
+
+print("done")
