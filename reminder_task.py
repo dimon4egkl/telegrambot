44fe5cf20788836119c8b06021819b6a26b87ee5
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-01 21:38:26 +0300

    fix time zone problem with task reminder

--- reminder_task.py
+++ reminder_task.py
@@ -7,17 +7,16 @@ import datetime
 bot = telebot.TeleBot(config.TOKEN)
 db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
-# while True:
+#while True:
 date = datetime.datetime.now().time()
-hour = date.hour #+ 7
+hour = date.hour + 7
     # додати 7 годин коли закідати в git
 if hour == 18 or hour ==21:
     sql.execute("SELECT id, present_day_task_completeness FROM users")
     users = sql.fetchall()
     for user in users:
-        #if user[0] == 404063513 :
         if user[1] == 0 :
+            print("delivered")
             bot.send_message(user[0], "Пссс Пссс. Тут тебе цей... завдання чекає. Рекомендую приділити пару хвилин, щоб його виконати. А тоді повертайся до своїх корисних і приємних справ")
-   # time.sleep()
+   # time.sleep(3600)
 
-print("done")
