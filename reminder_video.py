fc755adc230a29673a63fe7001981be13afc5c60
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-01 21:55:09 +0300

    add task and video reminders and task processing

---
+++ reminder_video.py
@@ -0,0 +1,30 @@
+import time
+import sqlite3
+import telebot
+import config
+import datetime
+
+bot = telebot.TeleBot(config.TOKEN)
+db = sqlite3.connect("server.sqlite3")
+sql = db.cursor()
+while True:
+    date = datetime.datetime.now().time()
+    hour = date.hour + 7
+    if hour > 24:
+        hour -= 24
+    minute = date.minute
+    # додати 7 годин коли закідати в git
+    sql.execute("SELECT id, wakeup_completeness, hour_of_wakeup, minute_of_wakeup  FROM users")
+    users = sql.fetchall()
+
+
+    for user in users:
+            differense_in_time = (user[2] - hour)*60 + user[3] - minute
+            print(hour)
+            print(minute)
+            print(differense_in_time)
+            if differense_in_time <= 15 and differense_in_time > 0:
+                bot.send_message(user[0], "До скидання відео звіту залишилось всього {0} хв. Поспіши :)".format(differense_in_time))
+    time.sleep(300)
+
+
--- tasks_process.py
+++ tasks_process.py
@@ -1,24 +1,21 @@
 import sqlite3
 import datetime
-
+import time
 db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
 
-# sql.execute("SELECT id, present_day_task_completeness, wakeup_completeness FROM users")
-# #
-# # tasks = sql.fetchall()
-# # for task in tasks:
-# #     print(task)
-# #     sql.execute("UPDATE users SET prev_day_task_completeness =?, present_day_task_completeness=?  WHERE id =? ",(task[1],0,task[0],))
-# #     db.commit()
-# # print("done")
 
-#sql.execute("SELECT id, present_day_task_completeness, wakeup_completeness FROM users")
-sql.execute("SELECT * FROM users")
-tasks = sql.fetchall()
-for task in tasks:
-    print(task)
-    if task[9]=="Назар":
-         sql.execute("UPDATE users SET wakeup_completeness =?  WHERE id =? ",(1,task[0],))
-    db.commit()
-print("done")
+while True:
+    sql.execute("SELECT id, present_day_task_completeness, wakeup_completeness FROM users")
+    tasks = sql.fetchall()
+    date = datetime.datetime.now().time()
+    hour = date.hour + 7
+    if hour > 24:
+        hour -= 24
+    if hour == 2:
+        for task in tasks:
+            sql.execute("UPDATE users SET prev_day_task_completeness =?, present_day_task_completeness=?,wakeup_completeness=?  WHERE id =? ",(task[1],0,0,task[0],))
+            db.commit()
+    time.sleep(3600)
+
+
