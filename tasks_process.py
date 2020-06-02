625113c23a26c4219718c77627b3dcadf7894efe
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-02 06:11:27 +0300

    fix tsk processing

--- tasks_process.py
+++ tasks_process.py
@@ -9,13 +9,16 @@ while True:
     sql.execute("SELECT id, present_day_task_completeness, wakeup_completeness FROM users")
     tasks = sql.fetchall()
     date = datetime.datetime.now().time()
-    hour = date.hour + 7
+    hour = date.hour  + 7
     if hour > 24:
         hour -= 24
-    if hour == 2:
+    print(hour)
+    if hour == 6:
+
         for task in tasks:
             sql.execute("UPDATE users SET prev_day_task_completeness =?, present_day_task_completeness=?,wakeup_completeness=?  WHERE id =? ",(task[1],0,0,task[0],))
             db.commit()
+    print("done")
     time.sleep(3600)
 
 
