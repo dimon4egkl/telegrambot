eb4f305bdfc24d3762d0b0431a89e38f36da78e3
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-04 12:12:39 +0300

    update show info to print task

--- show_info.py
+++ show_info.py
@@ -5,7 +5,12 @@ db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
 
 sql.execute("SELECT * FROM users")
-tasks = sql.fetchall()
-for task in tasks:
-    print(task)
+users = sql.fetchall()
+print("users")
+for user in users:
+    print(user)
+print("task")
+sql.execute("SELECT text FROM tasks")
+task = sql.fetchone()
+print(task)
 print("done")
