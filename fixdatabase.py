fe0fdf79d4841a033bc358da191fc616c410064a
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-04 11:48:32 +0300

    insert 1 task

--- fixdatabase.py
+++ fixdatabase.py
@@ -3,5 +3,5 @@ import sqlite3
 db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
 sql.execute("CREATE TABLE IF NOT EXISTS tasks (id integer PRIMARY KEY,text text NOT NULL)");
-
+sql.execute("INSERT INTO tasks VALUES (1,'-'")
 print("done")
