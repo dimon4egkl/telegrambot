34a22b25f19fd93d862b86682dc8a8c15d122070
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-04 11:51:39 +0300

    fix request

--- fixdatabase.py
+++ fixdatabase.py
@@ -2,6 +2,7 @@ import sqlite3
 
 db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
-sql.execute("CREATE TABLE IF NOT EXISTS tasks (id integer PRIMARY KEY,text text NOT NULL)");
-sql.execute("INSERT INTO tasks VALUES (1,'-'")
-print("done")
+sql.execute("CREATE TABLE IF NOT EXISTS tasks (id integer PRIMARY KEY,text text NOT NULL)")
+sql.execute("INSERT INTO tasks VALUES (1,'-')")
+db.commit()
+print("inserted")
