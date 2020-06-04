c13cb4954615fcb517fafee63157df7d0929d95c
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-04 09:14:09 +0300

    Kate is not is report, add task_completeness for Taya

--- fixdatabase.py
+++ fixdatabase.py
@@ -3,7 +3,7 @@ import sqlite3
 db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
 
-sql.execute("UPDATE users SET prev_day_task_completeness=1")
+sql.execute("UPDATE users SET prev_day_task_completeness=1 WHERE id=?",(171973325,))
 db.commit()
 
 print("done")
--- report.py
+++ report.py
@@ -10,7 +10,7 @@ db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
 while True:
     date = datetime.datetime.now().time()
-    hour = date.hour + 7
+    hour = date.hour + 1
         # додати 7 годин коли закідати в git
     if hour == 10:
         sql.execute("SELECT * FROM users")
@@ -22,36 +22,38 @@ while True:
         def report(report_text):
             wokeup_in_time = 0
             for user in users:
-                name = ""
-                if user[1] == None or user[1] =="" or user[1]==" ":
-                    if user[10]==None:
-                        name = user[9]
+                if user[1]!="katarzynka":
+                    name = ""
+                    if user[1] == None or user[1] =="" or user[1]==" ":
+                        if user[10]==None:
+                            name = user[9]
+                        else:
+                            name = user[9]+ " " + user[10]
                     else:
-                        name = user[9]+ " " + user[10]
+                        name = "@"+ user[1]
+                    task_text = ""
+                    if user[6]==0:
+                        task_text= " Завдання не виконано ❌"
+                    if int(user[8]) == 0:
+                        insert_index = report_text.find("&&")
+                        insert_text = name + " -відео не було надіслане до 10:00 ❌".format(user)
+                        report_text = report_text[:insert_index] + insert_text +task_text+ "\n" + report_text[insert_index:]
+                    if int(user[8]) == 2:
+                        insert_index = report_text.find("&&")
+                        insert_text = name + " -відео надіслане пізніше {0[4]}:{0[5]}❌".format(user)
+                        report_text = report_text[:insert_index] + insert_text +task_text +"\n" + report_text[insert_index:]
+                    if int(user[8]) == 1 and user[6]==0:
+                        insert_index = report_text.find("&&")
+                        insert_text = name+task_text
+                        report_text = report_text[:insert_index] + insert_text +"\n" +report_text[insert_index:]
+                        wokeup_in_time+=1
+                    if int(user[8]) == 1 and user[6]!=0:
+                        insert_index = report_text.find("&")
+                        insert_text = name+" ✅"
+                        report_text = report_text[:insert_index] + insert_text +"\n" +report_text[insert_index:]
+                        wokeup_in_time+=1
                 else:
-                    name = "@"+ user[1]
-                task_text = ""
-                if user[6]==0:
-                    task_text= " Завдання не виконано ❌"
-                if int(user[8]) == 0:
-                    insert_index = report_text.find("&&")
-                    insert_text = name + " -відео не було надіслане до 10:00 ❌".format(user)
-                    report_text = report_text[:insert_index] + insert_text +task_text+ "\n" + report_text[insert_index:]
-                if int(user[8]) == 2:
-                    insert_index = report_text.find("&&")
-                    insert_text = name + " -відео надіслане пізніше {0[4]}:{0[5]}❌".format(user)
-                    report_text = report_text[:insert_index] + insert_text +task_text +"\n" + report_text[insert_index:]
-                if int(user[8]) == 1 and user[6]==0:
-                    insert_index = report_text.find("&&")
-                    insert_text = name+task_text
-                    report_text = report_text[:insert_index] + insert_text +"\n" +report_text[insert_index:]
-                    wokeup_in_time+=1
-                if int(user[8]) == 1 and user[6]!=0:
-                    insert_index = report_text.find("&")
-                    insert_text = name+" ✅"
-                    report_text = report_text[:insert_index] + insert_text +"\n" +report_text[insert_index:]
-                    wokeup_in_time+=1
-
+                    pass
             insert_index = report_text.find("&")
             report_text = report_text[:insert_index] +report_text[insert_index+1:]
             insert_index = report_text.find("&&")
