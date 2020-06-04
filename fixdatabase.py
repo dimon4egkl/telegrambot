b9e4e8a6a00c1e9958f594a87b26a60b7f354161
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-06-04 11:44:29 +0300

    add new table for task text

--- fixdatabase.py
+++ fixdatabase.py
@@ -2,8 +2,6 @@ import sqlite3
 
 db = sqlite3.connect("server.sqlite3")
 sql = db.cursor()
-
-sql.execute("UPDATE users SET prev_day_task_completeness=1 WHERE id=?",(171973325,))
-db.commit()
+sql.execute("CREATE TABLE IF NOT EXISTS tasks (id integer PRIMARY KEY,text text NOT NULL)");
 
 print("done")
